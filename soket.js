9fdcbfa517e1952fc8c795357d813fc7e22138de
meir <m.stigel@gmail.com>
2020-12-21 11:37:02 +0200

    make it better

---
+++ soket.js
@@ -0,0 +1,19 @@
+console.log('runing');
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '\\index.html');
+})
+
+
+io.sockets.on('connection', socket => {
+    socket.on('input', ms => {
+        io.sockets.emit('new', ms);
+    })
+})
+
+
+
+server.listen(5000);
