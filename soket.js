8e9c026efd13d277f488ff729783ec5cddec42b9
misternet101 <76657475+misternet101@users.noreply.github.com>
2020-12-28 14:58:01 +0200

    Update soket.js

--- soket.js
+++ soket.js
@@ -6,7 +6,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '\\index.html');
+    res.sendFile(__dirname + '/index.html');
 })
 
 
